149e5855c8557189bc641cad7c2366b1c5e9cbb3
xunruan <xunruan@icloud.com>
2022-09-06 02:16:21 -0400

    Add signup page test

--- django_project/accounts/tests.py
+++ django_project/accounts/tests.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse, resolve
+
+from accounts.forms import CustomUserCreationForm
+from accounts.views import SignupPageView
 
 
 # Create your tests here.
@@ -25,3 +29,27 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignUpPageTests(TestCase):
+    def setUp(self) -> None:
+        url = reverse("signup")
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        # test if url loads a template of our choice
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "registration/signup.html")
+        self.assertContains(self.response, "Sign Up")
+        self.assertNotContains(self.response, "tfothaohva")
+
+    def test_signup_form(self):
+        # test if url loads a form of our choice
+        form = self.response.context.get("form")
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, "csrfmiddlewaretoken")
+
+    def test_signup_view(self):
+        # test if url calls cbv (class-based view)
+        view = resolve("/accounts/signup/")
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
